29d6e94ad0423ee1fdf697dae2868b527de441e6
Harley Bussell <harley@harley-desktop.(none)>
2009-08-10 12:42:48 +1000

    fix create-site comments

--- create-site.sh
+++ create-site.sh
@@ -2,9 +2,8 @@
 git clone git://github.com/hbussell/pinax-satchmo-buildout.git  /tmp/pinax-satchmo-buildout
 cp /tmp/pinax-satchmo-buildout/buildout.cfg .
 
-# bring in website settings.py, satchmo_settings.py, urls.py
+# bring in sample project settings and templates
 # if you comment out this step a default project will be created by the djangorecipe.
-# you will then need to manually copy in the settings files 
 cp -R /tmp/pinax-satchmo-buildout/website/ .
 
 virtualenv --no-site-packages env
